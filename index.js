27e393a506e9230439b72a9ac1dc5628955c4543
florence <loracodes3@gmail.com>
2024-10-15 16:41:06 +0300

    bng

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
